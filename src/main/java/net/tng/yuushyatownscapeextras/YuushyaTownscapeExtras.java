54672418d8518c8bfea2621ab22b5d16b55be379
Sricharan Rajkumar <sricharanrajkumar@Sricharans-MacBook-Pro.local>
2026-01-17 17:39:35 +1000

    Initial commit

---
+++ src/main/java/net/tng/yuushyatownscapeextras/YuushyaTownscapeExtras.java
@@ -0,0 +1,16 @@
+package net.tng.yuushyatownscapeextras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class YuushyaTownscapeExtras implements ModInitializer {
+	public static final String MOD_ID = "yuushya-townscape-extras";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/tng/yuushyatownscapeextras/YuushyaTownscapeExtrasClient.java
@@ -0,0 +1,10 @@
+package net.tng.yuushyatownscapeextras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YuushyaTownscapeExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
